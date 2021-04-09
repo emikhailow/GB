066efee34db5e92d6d1624d0a68f56ed8cd0af26
Evgenii Mikhailov <eugene.mikhailow@gmail.com>
2021-04-09 12:46:13 +0300

    Домашняя работа по уроку 5

---
+++ src/main/java/GB5_HomeWork/Employee.java
@@ -0,0 +1,84 @@
+package GB5_HomeWork;
+
+public class Employee {
+
+    private String name;
+    private String postition;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public static boolean isOlderThan(Employee employee, int age){
+        return employee.age > age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", postition='" + postition + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public Employee(String name, String postition, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.postition = postition;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostition() {
+        return postition;
+    }
+
+    public void setPostition(String postition) {
+        this.postition = postition;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
